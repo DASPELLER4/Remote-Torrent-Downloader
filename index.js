ecf3dfab0e2aa1a6b111cd47165c379be6b55d24
JakeT23 <69044307+JakeT23cool@users.noreply.github.com>
2022-08-29 13:44:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function getDir(dir,level,downloading,parentdir){
                         if(fs.lstatSync(dir+"/"+file).isDirectory()){
                                 if(parentdir==""){
                                         p = sanitiseNames(file);
-                                        response+="<p style=\"" + isAvailable + "white-space: nowrap; display:inline; margin-left: " + (30*level).toString() + "px\">📁"+file+"</p> <a style=\"color: blue; text-decoration: underline;\" onclick=\"" + sanitiseNames(file) + "FUNC1345();\">Collapse</a><br><script>var " + sanitiseNames(file) + "Toggle=true; function " + sanitiseNames(file) + "FUNC1345(){if(" + sanitiseNames(file) + "Toggle){Array.from(document.getElementsByClassName(\"" + sanitiseNames(file) + "\")).forEach((item, index) => {item.style.visibility = \"hidden\"; item.style.lineHeight=\"0\";});}else{Array.from(document.getElementsByClassName(\"" + sanitiseNames(file) + "\")).forEach((item, index) => {item.style.visibility = \"visible\"; item.style.lineHeight=\"1\";});}" + sanitiseNames(file) + "Toggle = !" + sanitiseNames(file) + "Toggle;}</script>";
+                                        response+="<p style=\"" + isAvailable + "white-space: nowrap; display:inline; margin-left: " + (30*level).toString() + "px\">📁"+file+"</p> <a style=\"color: blue; text-decoration: underline;\" onclick=\"collapse('"+sanitiseNames(file)+"');\">↩️</a><br>";
                                 }else{
                                         p = parentdir;
                                         response+="<p class=\"" + p + "\" style=\"" + isAvailable + "white-space: nowrap; display:inline; margin-left: " + (30*level).toString() + "px\">>📁"+file+"</p><br>";
@@ -56,7 +56,8 @@ app.post('/submit', function(req,res){
 	}
 	res.redirect('/list');
 	const { exec } = require("child_process");
-	exec("aria2c --seed-ratio=1.0 \'" + req.body.magnet + "\' -d \'public/\'", (error, stdout, stderr) => {
+        console.log("Downloading ", req.body.magnet)
+	exec("aria2c --seed-ratio=1 \'" + req.body.magnet + "\' -d \'public/\'", (error, stdout, stderr) => {
 		if (error) {
 			console.log(error.message);
 			return;
@@ -70,10 +71,13 @@ app.post('/submit', function(req,res){
 });
 
 app.get('/list', function(req,res){
-	var response = "<h1>Downloads</h2><p>If you just got sent here from pressing \"submit\", do not worry if your files are not here yet, it can take some time to start the download</p><p>If your file is red, it is still downloading! You can refresh the page later</p><hr>"+getDir(path.join(__dirname+'/public'), 0, false, "");
-	res.send(response);
+	var Download_prompt = "<h1>Downloads</h2><p>If you just got sent here from pressing \"submit\", do not worry if your files are not here yet, it can take some time to start the download</p><p>If your file is red, it is still downloading! You can refresh the page later</p><hr>";
+	var Collapse_script = '<script>let folders = new Map();function collapse(key) {if (!(folders.has(key))){folders.set(key, true)}if (folders.get(key)) {Array.from(document.getElementsByClassName(key)).forEach((item, index) => {item.style.visibility = "hidden";item.style.lineHeight = "0";});} else {Array.from(document.getElementsByClassName(key)).forEach((item, index) => {item.style.visibility = "visible";item.style.lineHeight = "1";});}folders.set(key, !folders.get(key));};</script>';
+        var Files = getDir(path.join(__dirname+'/public'), 0, false, "")
+        res.send(Download_prompt+Collapse_script+Files);
 });
 
 app.use('/public', express.static(__dirname + '/public'));
 
 var server = app.listen(80);
+console.log("Live at http://127.0.0.1:80" );
